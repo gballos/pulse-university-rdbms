0c20b2f329cbd008bf0b865958bed4396540e4f9
gballos <ballos.ge@gmail.com>
2025-03-10 01:02:20 +0200

    Bug fixed install.sql

--- sql/install.sql
+++ sql/install.sql
@@ -2,25 +2,27 @@ DROP SCHEMA IF EXISTS pulse_uni_db;
 CREATE DATABASE pulse_uni_db;
 USE pulse_uni_db;
 
+-- Fixed column name in CHECK constraint and coordinate precision
 DROP TABLE IF EXISTS LOCATIONS;
 CREATE TABLE LOCATIONS (
-	location_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    address VARCHAR(50) CHECK(adress like '%, %'),   -- formatted as Street, Number?
+    location_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    address VARCHAR(50) CHECK(address LIKE '%, %'), 
     city VARCHAR(50),
     country VARCHAR(50),
     continent VARCHAR(50),
-    longtitude NUMERIC(5, 2),
-    latitude NUMERIC(5, 2),
+    longitude NUMERIC(9, 6), 
+    latitude NUMERIC(8, 6),   
     PRIMARY KEY(location_id)
 );
 
 DROP TABLE IF EXISTS FESTIVALS;
 CREATE TABLE FESTIVALS (
-	festival_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    festival_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     date_starting DATE,
     date_ending DATE,
-    duration INT,  -- having duration in minutes?
+    duration INT,
     location_id INT UNSIGNED,
     PRIMARY KEY(festival_id),
-    FOREIGN KEY(location_id) REFERENCES LOCATIONS(location_id)
+    FOREIGN KEY(location_id) REFERENCES LOCATIONS(location_id),
+    CHECK (date_ending > date_starting) 
 );
