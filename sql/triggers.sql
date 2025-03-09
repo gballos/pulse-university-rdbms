f7e17f7c150d13a8a2fcca64696bad6c143c517d
gballos <ballos.ge@gmail.com>
2025-03-09 14:14:49 +0200

    Delete triggers for BANDS and ARTISTS

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,1 +1,18 @@
+USE pulse_uni_db;
+
+DROP TRIGGER IF EXISTS delete_performance_after_artist
+DELIMITER //
+CREATE TRIGGER delete_performance_after_artist
+AFTER DELETE ON ARTISTS
+FOR EACH ROW
+DELETE FROM PERFORMANCES WHERE performer_id = OLD.artist_id AND is_solo = 1;
+//
+
+DROP TRIGGER IF EXISTS delete_performance_after_band
+DELIMITER //
+CREATE TRIGGER delete_performance_after_band
+AFTER DELETE ON BANDS
+FOR EACH ROW
+DELETE FROM PERFORMANCES WHERE performer_id = OLD.band_id AND is_solo = 0;
+//
 
