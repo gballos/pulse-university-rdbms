6ca0d23b15f6c0f3a971c7004d29c2aa487c1082
gballos <ballos.ge@gmail.com>
2025-04-23 14:18:08 +0300

    Minor fix

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -33,15 +33,18 @@ BEGIN
   DECLARE ticket_for_resale_id INT;
   SELECT ticket_for_resale_id INTO ticket_for_resale_id
   FROM TICKETS_FOR_RESALE
-  WHERE ticket_id = (SELECT ticket_id FROM TICKETS WHERE ticket_id = ticket_id) 
+  WHERE ticket_id = (SELECT ticket_id FROM TICKETS WHERE ticket_id = NEW.ticket_id)
     OR (ticket_category = (SELECT ticket_category FROM TICKETS WHERE ticket_category = NEW.ticket_category) 
     AND event_id = (SELECT event_id FROM FESTIVAL_EVENTS WHERE event_id = NEW.event_id))
   ORDER BY ticket_for_resale ASC
   LIMIT 1;
 
   IF buyer_id IS NOT NULL AND ticket_for_resale_id IS NOT NULL THEN 
-    DELETE FROM BUYERS WHERE buyer_id = buyer_id LIMIT 1;
+    DELETE FROM BUYERS WHERE buyer_id = NEW.buyer_id LIMIT 1;
     DELETE FROM TICKETS_FOR_RESALE WHERE tickets_for_resale_id = tickets_for_resale_id LIMIT 1;
   END IF;
 END; 
 //
+
+
+
