32e8279816b585e40bab771c037493fbf0403eb3
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-08 14:56:10 +0300

    Created create.py

---
+++ data/create.py
@@ -0,0 +1,21 @@
+import random 
+import faker
+import datetime
+
+N_LOCATIONS = 100
+N_FESTIVALS = 100
+N_STAGES = 150
+N_TECHNICAL_SUPPLIES = 50
+N_EVENTS = 100
+N_ARTISTS = 200
+N_BANDS = 100
+N_PERFORMANCES = 100
+N_STAFF = 200
+N_VISITORS = 300
+N_TICKETS = 350
+N_REVIEWS = 300
+N_BUYERS = 200
+N_RESALE_TICKETS = 50
+
+random.seed(42)
+faker.Faker.seed(42)
