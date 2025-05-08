a51070e76be0106a203a44400079f14565629ab0
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-08 14:56:56 +0300

    Function to create fake LOCATIONS data

--- data/create.py
+++ data/create.py
@@ -19,3 +19,40 @@ N_RESALE_TICKETS = 50
 
 random.seed(42)
 faker.Faker.seed(42)
+
+# LOCATIONS
+def fake_locations(f):
+    continents = ['Asia', 
+    'Europe', 
+    'North America', 
+    'South America', 
+    'Africa', 
+    'Australia', 
+    'Antarctica']
+
+    fake = faker.Faker()
+
+    def build_locations(location_id):
+        address = fake.address()
+        city = fake.city()
+        country = fake.country()
+        continent = random.choice(continents)
+        longtitude = fake.longitude()
+        latitude = fake.latitude()
+        image = fake.image_url()
+        return f"INSERT INTO LOCATIONS (location_id, address, city, country, continent, longtitude, latitude, image) VALUES ('{
+            location_id}', '{
+            address}', '{
+            city}', '{
+            country}', '{
+            continent}', '{
+            longtitude}', '{
+            latitude}', '{
+            image}');\n"
+
+    locations = (build_locations(_) for _ in range(1, N_LOCATIONS+1))
+
+    for location in locations:
+        f.write(location)
+
+  
