d9e20dbde0071b5d83c066414046b22982407377
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-12 02:26:06 +0300

    Create Q06.sql

---
+++ sql/queries/Q06.sql
@@ -0,0 +1,21 @@
+USE pulse_uni_db;
+
+SET @visitor_id = 42;
+
+--simple query
+SELECT
+    r.visitor_id,
+    r.performance_id,
+    p.event_id,
+    p.is_solo,
+    ROUND(AVG(
+        r.interpretation_rating +
+        r.sound_lighting_rating +
+        r.stage_presence_rating +
+        r.organization_rating +
+        r.overall_impression_rating
+    ) / 5, 2) AS avg_rating
+FROM REVIEWS r
+JOIN PERFORMANCES p ON r.performance_id = p.performance_id
+WHERE r.visitor_id = @visitor_id
+GROUP BY r.visitor_id, r.performance_id, p.event_id, p.is_solo;
