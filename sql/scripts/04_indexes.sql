f4bf305453152e8f9c52245e48e4cc36f340fc01
P. Vasileiou <vasileioupanagiota4@gmail.com>
2025-05-11 18:36:48 +0300

    added idx events-performers & visitor-events

--- sql/scripts/04_indexes.sql
+++ sql/scripts/04_indexes.sql
@@ -9,3 +9,12 @@ CREATE INDEX idx_events_staff ON STAFF(event_id);
 -- artist / band relationships
 CREATE INDEX idx_ab_artist ON ARTISTS_X_BANDS(artist_id);
 CREATE INDEX idx_ab_band ON ARTISTS_X_BANDS(band_id);
+
+-- composite indexes 
+
+-- for queries 2,3,4,10,15  /  require the connection both events and performers
+CREATE INDEX idx_events_performers ON PERFORMANCES(event_id, performer_id);
+
+-- for queries 6,9,15  /  require both visitors and events 
+-- useful for the big number of tickets
+CREATE INDEX idx_tickets_visitor_event ON TICKETS(visitor_id, event_id);
