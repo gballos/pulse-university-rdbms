f92bf2c36dea834d21321bb4014db3836ef0d0df
P. Vasileiou <vasileioupanagiota4@gmail.com>
2025-05-11 18:10:42 +0300

    indexes artists-bands

--- sql/scripts/04_indexes.sql
+++ sql/scripts/04_indexes.sql
@@ -5,3 +5,7 @@ CREATE INDEX idx_events_festivals ON FESTIVAL_EVENTS(festival_id);
 CREATE INDEX idx_events_performances_events ON PERFORMANCES(event_id);
 CREATE INDEX idx_events_tickets ON TICKETS(event_id);
 CREATE INDEX idx_events_staff ON STAFF(event_id);
+
+-- artist / band relationships
+CREATE INDEX idx_ab_artist ON ARTISTS_X_BANDS(artist_id);
+CREATE INDEX idx_ab_band ON ARTISTS_X_BANDS(band_id);
