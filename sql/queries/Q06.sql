870123a33d4013e65d92297dde4983c54bf7d87d
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-12 02:27:31 +0300

    Updated Q06.sql to include query with FORCE INDEX

--- sql/queries/Q06.sql
+++ sql/queries/Q06.sql
@@ -19,3 +19,20 @@ FROM REVIEWS r
 JOIN PERFORMANCES p ON r.performance_id = p.performance_id
 WHERE r.visitor_id = @visitor_id
 GROUP BY r.visitor_id, r.performance_id, p.event_id, p.is_solo;
+
+-- query with FORCE INDEX
+SELECT
+    r.visitor_id,
+    r.performance_id,
+    p.event_id,
+    p.is_solo,
+    ROUND(AVG(
+        r.interpretation_rating +
+        r.sound_lighting_rating +
+        r.stage_presence_rating +
+        r.organization_rating +
+        r.overall_impression_rating
+    ) / 5, 2) AS avg_rating
+FROM REVIEWS r FORCE INDEX (idx_reviews_visitors_performances)
+WHERE r.visitor_id = @visitor_id
+GROUP BY r.visitor_id, r.performance_id;
