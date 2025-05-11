494da8d35f846f2c3b086ffa82359c6bc73f91de
gballos <ballos.ge@gmail.com>
2025-05-11 20:15:06 +0300

    Added band performances

--- sql/queries/Q15.sql
+++ sql/queries/Q15.sql
@@ -1,11 +1,36 @@
+WITH solo_scores AS(
+    SELECT
+        r.visitor_id AS visitor_id,
+        p.performer_id AS artist_id,
+        SUM(r.interpretation_rating+r.interpretation_rating) AS score
+    FROM REVIEWS r
+    JOIN PERFORMANCES p on r.performance_id = p.performance_id
+    WHERE p.is_solo = 1
+    GROUP BY visitor_id, artist_id
+),
+band_scores AS(
+    SELECT
+        r.visitor_id AS visitor_id,
+        ab.artist_id AS artist_id,
+        SUM(r.interpretation_rating+r.interpretation_rating) AS score
+    FROM REVIEWS r
+    JOIN PERFORMANCES p on r.performance_id = p.performance_id
+    JOIN ARTISTS_X_BANDS ab on ab.band_id = p.performer_id
+    WHERE p.is_solo = 0
+    GROUP BY visitor_id, ab.artist_id
+),
+all_scores AS(
+    SELECT * FROM band_scores
+    UNION ALL
+    SELECT * FROM solo_scores
+)
 SELECT
     CONCAT(v.first_name, ' ', v.last_name) as visitor_name,
     CONCAT(a.first_name, ' ', a.last_name) as artist_name,
-    SUM(r.interpretation_rating + r.overall_impression_rating) as total_score
-FROM REVIEWS r
-JOIN VISITORS v ON r.visitor_id = v.visitor_id
-JOIN PERFORMANCES p ON r.performance_id = p.performance_id
-JOIN ARTISTS a on p.performer_id = a.artist_id WHERE p.is_solo = 1
-GROUP BY v.visitor_id, a.artist_id
+    SUM(score) as total_score
+FROM all_scores s
+JOIN VISITORS v ON s.visitor_id = v.visitor_id
+JOIN ARTISTS a ON s.artist_id = a.artist_id
+GROUP BY s.visitor_id, s.artist_id
 ORDER BY total_score DESC
-LIMIT 5;
+LIMIT 5
