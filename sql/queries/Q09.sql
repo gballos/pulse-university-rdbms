fd22dad468fb1896a074bcd22440f5a0ee1d7e4a
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-13 21:44:58 +0300

    Updated Q09.sql

--- sql/queries/Q09.sql
+++ sql/queries/Q09.sql
@@ -7,10 +7,13 @@ SELECT
     v.last_name,
     YEAR(fe.event_date) AS year,
     COUNT(*) AS num_performances
+    
 FROM TICKETS t
 JOIN VISITORS v on t.visitor_id = v.visitor_id
 JOIN FESTIVAL_EVENTS fe ON t.event_id = fe.event_id
+
 WHERE t.is_scanned = 1
+
 GROUP BY t.visitor_id, YEAR(fe.event_date), fe.festival_id
 HAVING COUNT(*) > 3
 ORDER BY num_performances DESC;
