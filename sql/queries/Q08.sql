be0fe12035db57417409778c0e0a37d7ee54c9fa
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-13 19:15:49 +0300

    Updated Q08.sql

--- sql/queries/Q08.sql
+++ sql/queries/Q08.sql
@@ -6,8 +6,10 @@ SELECT
     s.staff_id,
     s.first_name,
     s.last_name
+    
 FROM STAFF s
 JOIN STAFF_CATEGORIES sc ON s.category_id = sc.staff_category_id
+    
 WHERE sc.technical_id IS NULL
   AND s.staff_id NOT IN (
     SELECT s2.staff_id
