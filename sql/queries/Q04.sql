9d1feb628d25b843982162cd715a505ff0501083
gballos <ballos.ge@gmail.com>
2025-05-13 19:37:29 +0300

    modified:   ../../sql/queries/Q04.sql

--- sql/queries/Q04.sql
+++ sql/queries/Q04.sql
@@ -1,9 +1,9 @@
 USE pulse_uni_db;
 
-SET @artist_id = 500;
+SET @artist_id = 41;
 
 -- simple query 
--- EXPLAIN ANALYZE
+EXPLAIN ANALYZE
 SELECT
     a.artist_id,
     a.first_name,
@@ -17,12 +17,13 @@ WHERE a.artist_id = @artist_id
 GROUP BY a.artist_id;
 
 -- query with FORCE INDEX
+EXPLAIN ANALYZE
 SELECT
     a.artist_id,
     a.first_name,
     a.last_name,
-    ROUND(AVG(r.interpretation_rating), 2),
-    ROUND(AVG(r.overall_impression_rating), 2)
+    ROUND(AVG(r.interpretation_rating), 2) AS avg_interpretation,
+    ROUND(AVG(r.overall_impression_rating), 2) AS avg_impression
 FROM REVIEWS r FORCE INDEX (idx_reviews_performance)
 JOIN PERFORMANCES p FORCE INDEX (idx_performances_performer)
     ON r.performance_id = p.performance_id
