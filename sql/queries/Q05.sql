a372fc5ea718c2f9afe2f5c8e8abf76bae8ade0f
Marlena Panagiotakou <158483792+MarlenaPanagiotakou@users.noreply.github.com>
2025-05-13 17:22:25 +0300

    Updated Q05.sql with comments

--- sql/queries/Q05.sql
+++ sql/queries/Q05.sql
@@ -6,8 +6,10 @@ SELECT
     a.last_name,
     a.birthday,
     COUNT(DISTINCT artist_festivals.festival_id) AS festival_count
+    
 FROM ARTISTS a
 LEFT JOIN (
+    -- For solo artists
     SELECT p.performer_id AS artist_id, fe.festival_id
     FROM PERFORMANCES p
     JOIN FESTIVAL_EVENTS fe ON fe.event_id = p.event_id
@@ -16,6 +18,7 @@ LEFT JOIN (
 
     UNION ALL
 
+    -- For bands
     SELECT ab.artist_id as artist_id, fe.festival_id
     FROM PERFORMANCES p
     JOIN FESTIVAL_EVENTS fe ON fe.event_id = p.event_id
@@ -23,8 +26,7 @@ LEFT JOIN (
     JOIN FESTIVALS f on fe.festival_id = f.festival_id
     WHERE p.is_solo = 0
 ) AS artist_festivals ON artist_festivals.artist_id = a.artist_id
+    
 WHERE TIMESTAMPDIFF(YEAR, a.birthday, CURDATE()) < 30
 GROUP BY a.artist_id
 ORDER BY festival_count DESC;
-
-
