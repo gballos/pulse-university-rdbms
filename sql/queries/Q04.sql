d75298bd8f1edc3579df231b8839d71838641b0b
gballos <ballos.ge@gmail.com>
2025-05-14 22:45:34 +0300

    Final

--- sql/queries/Q04.sql
+++ sql/queries/Q04.sql
@@ -16,6 +16,7 @@ JOIN ARTISTS a ON p.performer_id = a.artist_id AND p.is_solo = 1
 WHERE a.artist_id = @artist_id
 GROUP BY a.artist_id;
 
+
 -- query with FORCE INDEX
 EXPLAIN ANALYZE
 SELECT
@@ -27,6 +28,6 @@ SELECT
 FROM REVIEWS r FORCE INDEX (idx_reviews_performance)
 JOIN PERFORMANCES p FORCE INDEX (idx_performances_performer)
     ON r.performance_id = p.performance_id
-JOIN ARTISTS a ON p.performer_id = a.artist_id AND p.is_solo = 1
+STRAIGHT_JOIN ARTISTS a ON p.performer_id = a.artist_id AND p.is_solo = 1
 WHERE a.artist_id = @artist_id
 GROUP BY a.artist_id;
